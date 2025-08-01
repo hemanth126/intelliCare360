51f670d45427607928ea8c55f9ad30cd5d680e10
hemanth126 <hemanthsoldier7095@gmail.com>
2025-08-01 16:22:15 +0530

    IC360-7-git-repo - commit

--- intellicare360/src/main/java/com/dis/intellicare360/Intellicare360Application.java
+++ intellicare360/src/main/java/com/dis/intellicare360/Intellicare360Application.java
@@ -9,6 +9,8 @@ public class Intellicare360Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Intellicare360Application.class, args);
 //this is addeed 
+		
+		//puja chnaged comment 
 	}
 
 }
