cdb20baa7ff618d236055deaf2bd9b05586db9fa
hemanth126 <hemanthsoldier7095@gmail.com>
2025-08-01 11:59:20 +0530

    Added full IntelliCare360 Spring Boot backend project

---
+++ intellicare360/src/main/java/com/dis/intellicare360/Intellicare360Application.java
@@ -0,0 +1,13 @@
+package com.dis.intellicare360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intellicare360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Intellicare360Application.class, args);
+	}
+
+}
---
+++ intellicare360/src/test/java/com/dis/intellicare360/Intellicare360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dis.intellicare360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Intellicare360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
